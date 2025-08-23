a3ea9a0f6dd10c5895bc9ebb25483e0a443d2fdc
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-08-23 17:47:19 -0500

    implementacion del break y default (se me olvido)

--- src/Menu.java
+++ src/Menu.java
@@ -32,16 +32,56 @@ public class Menu {
                         switch (secondOption){
                             case 'A' :
                                 System.out.println("----- Registro Para Nuevo Usuario. -----");
+                                break;
                             case 'B' :
                                 System.out.println("----- Eliminar A Un Usuario. -----");
+                                break;
                             case 'C' :
                                 System.out.println("----- Modificar A Un Usuario. -----");
+                                break;
                             case 'D' :
                                 System.out.println("----- Expedir Lista De Usuarios. -----");
+                                break;
                             case 'S' :
                                 System.out.println("Hasta Luego...");
+                                break;
+                            default:
+                                System.out.println("Opcion No Valida...");
                         }
                     }while (secondOption != 'S');
+
+                case 2 :
+                    System.out.println("----- Gestion de Usuarios -----");
+                    char thirdOption;
+                    do {
+                        System.out.println("A. Registrar Ususario.");
+                        System.out.println("B. Eliminar Usuario.");
+                        System.out.println("C. Modificar Usuario.");
+                        System.out.println("D. Listar Usuarios.");
+                        System.out.println("S. Salir, Gestion de Usuarios. \n");
+                        System.out.println("Digite la Opcion aqui ↓: ");
+                        thirdOption = sc.next().toUpperCase().charAt(0); // lee -- pasa a mayuscula -- toma el primer caracter
+
+                        switch (thirdOption){
+                            case 'A' :
+                                System.out.println("----- Registro Para Nuevo Usuario. -----");
+                                break;
+                            case 'B' :
+                                System.out.println("----- Eliminar A Un Usuario. -----");
+                                break;
+                            case 'C' :
+                                System.out.println("----- Modificar A Un Usuario. -----");
+                                break;
+                            case 'D' :
+                                System.out.println("----- Expedir Lista De Usuarios. -----");
+                                break;
+                            case 'S' :
+                                System.out.println("Hasta Luego...");
+                                break;
+                            default:
+                                System.out.println("Opcion No Valida...");
+                        }
+                    }while (thirdOption != 'S');
 
             }
 
