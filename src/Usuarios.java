77a4b6562876792f271c89f39512fc5bab02c2c0
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-08-24 11:03:00 -0500

    tributos y contructor de la clase Usuarios

---
+++ src/Usuarios.java
@@ -0,0 +1,15 @@
+public class Usuarios {
+    private String name;
+    private String id;
+    private String email;
+
+    // constructor
+    public Usuarios(String name, String id, String email){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+
+
+}
