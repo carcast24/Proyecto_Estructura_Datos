f87d325995ec600d2924588da8140f53d76f14de
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-09-02 20:56:18 -0500

    implementacion de metodos de la interface Manager

--- src/UsuariosManager.java
+++ src/UsuariosManager.java
@@ -1,13 +1,45 @@
 import java.util.ArrayList;
+import java.util.List;
 
-public class UsuariosManager {
+public class UsuariosManager implements Managers<Usuarios> {
     private ArrayList<Usuarios> usuarios = new ArrayList<>();
 
-    //add
-    public void registroUsuario(Usuarios u){
+
+    @Override
+    public void agregar(Usuarios u) {
         usuarios.add(u);
     }
 
+    @Override
+    public boolean eliminar(String id) {
+        return eliminarUsuario(id);
+    }
+
+    @Override
+    public boolean modificar(String id, Usuarios nuevo) {
+        for(int i = 0; i < usuarios.size(); i++){
+            if(usuarios.get(i).getId().equalsIgnoreCase(id)){
+                usuarios.set(i,nuevo);
+            }
+            return true; // hizo el cambio
+        }
+        return false; // no encontro el id
+    }
+
+    @Override
+    public Usuarios buscar(String id) {
+        for (Usuarios buscar : usuarios){
+            if (buscar.getId().equalsIgnoreCase(id)){
+                return buscar;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List listar() {
+        return List.of();
+    }
     public boolean eliminarUsuario(String id){
         for (int i = 0; i < usuarios.size(); i++){
             if (usuarios.get(i).getId().equalsIgnoreCase(id)){ // ignora la forma en la que se escribe
@@ -18,7 +50,6 @@ public class UsuariosManager {
         }
         return false;
     }
-
-
+    // implimentar mi propio set
 
 }
